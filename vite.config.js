3b65f050a5e002d74fe3029bcc41a25cc7ad8fdb
souhaksou <98225683+souhaksou@users.noreply.github.com>
2025-05-22 01:47:27 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import masterCSS from '@master/css.vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    masterCSS()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
